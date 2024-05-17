2b1534829c5f1cd1728bc58104c27adefb050e92
poloman719 <92735647+poloman719@users.noreply.github.com>
2024-05-17 01:48:25 +0000

    added some server logic

--- server/index.js
+++ server/index.js
@@ -6,11 +6,15 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://10.0.0.45:5173'
+    origin: 'http://172.16.5.4:5173'
   }
 });
 
-server.listen(3000, '10.0.0.45', () => {
+const users = [];
+// structure: { name, id, health, position, velocity (not time, that is handled on server) }
+const gameState = false;
+
+server.listen(3000, () => {
   console.log("server running at http://10.0.0.45:3000");
 }); 
 
@@ -18,6 +22,12 @@ io.on('connection', (socket) => {
   console.log('someone connected')
   socket.on('disconnect', () => {
     console.log('disc')
+  })
+  socket.emit('users', users);
+  socket.on('new_user', (value) => {
+    const newUser = { name: value, id: socket.id, health: 100, velocity: null }
+    users += newUser;
+    io.emit('new_user', newUser)
   })
   socket.on('fire', (value) => {
     console.log(value)
