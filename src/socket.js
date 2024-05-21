38e4f21d4f3aa26acaf1cbaad5b7acbe994aacba
poloman719 <poloman7190@gmail.com>
2024-05-20 18:45:05 -0700

    made some bug fixes

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://multivar-server.onrender.com/';
+const URL = 'https://multivar-server.onrender.com/';
 
 export const socket = io(URL, {
-    autoConnect:false,
+    autoConnect: false,
     transports: ['websocket', 'polling', 'flashsocket']
 });
