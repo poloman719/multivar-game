059a315b80f4aecc3d01561ef144b344a703648c
poloman719 <poloman7190@gmail.com>
2024-05-23 03:19:44 -0700

    the one piece is real

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
     });
     socket.on("end_game", (winner) => {
       console.log(winner);
-      if (winner == "bruh" || winner=="null") {
+      if (winner == "bruh" || winner==null||winner==undefined) {
         updateBoard("The host has ended the game.");
         // console.log("host killed the game because they stink");
         let maxHits = 0;
